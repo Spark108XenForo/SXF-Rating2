6a1271318234b5b68e59cfbcd52560a33373f16f
Spark108 <kirito.asumo@gmail.com>
2018-02-27 14:41:47 +0300

    Fix Setup.php

--- upload/src/addons/SXFRating2/Setup.php
+++ upload/src/addons/SXFRating2/Setup.php
@@ -3,18 +3,22 @@
 namespace SXFRating2;
 
 use XF\AddOn\AbstractSetup;
+use XF\Db\Schema\Create;
+
 use XF\AddOn\StepRunnerInstallTrait;
 use XF\AddOn\StepRunnerUninstallTrait;
 use XF\AddOn\StepRunnerUpgradeTrait;
-use XF\Db\Schema\Alter;
 
 class Setup extends AbstractSetup
 {
-	public function install()
+	
+    use StepRunnerInstallTrait;
+    use StepRunnerUpgradeTrait;
+    use StepRunnerUninstallTrait;
+	
+	public function installStep1()
     {
-		$sm = $this->schemaManager();
-		
-        $sm->alterTable('xf_sxfr_rating', function(Alter $table)
+        $this->schemaManager()->createTable('xf_sxfr_rating', function(Create $table)
         {
             $table->addColumn('rating_id', 'VARBINARY', 50);
             $table->addColumn('title', 'VARCHAR', 50);
@@ -22,8 +26,11 @@ class Setup extends AbstractSetup
 			$table->addColumn('callback', 'VARCHAR', 100);
 			$table->addColumn('active', 'TINYINT')->setDefault(1);
         });
+	}
 		
-		$sm->query("
+	public function installStep2()
+	{
+		$this->db()->query("
 			INSERT INTO `xf_sxfr_rating` 
 				(`rating_id`, `title`, `icon`, `callback`, `active`) 
 			VALUES
@@ -32,14 +39,14 @@ class Setup extends AbstractSetup
 				(0x75736572706f696e7473, 'Top fot points', 'fa-trophy', 'SXFRating2\\Rating\\UserPoint', 1),
 				(0x757365727265736f75726365, 'Top for resources', 'fa-database', 'SXFRating2\\Rating\\UserResource', 1);
 		");
-    }
+	}
 
-    public function upgrade()
+    public function upgradeStep1()
     {
 
     }
 
-    public function uninstall()
+    public function uninstallStep1()
     {
         $this->schemaManager()->dropTable('xf_sxfr_rating');
     }
